b9a446b8a48bfc606d190fb23ba6a6d78a37ef48
MarcelKaemper <marcelkaemper862@gmail.com>
2019-03-04 16:13:23 +0100

    Renamed main.c to rectangle.c

--- rectangle.c
+++ rectangle.c
@@ -19,13 +19,13 @@ int main(int argc, char **argv){
 
 void render(void) {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glBegin(GL_TRIANGLES);
-		glColor3f(1,0,0);
-		glVertex2f(-0.5, -0.5);
-		glColor3f(0,1,0);
-		glVertex2f(0.5, -0.5);
-		glColor3f(0,0,1);
-		glVertex2f(0, 0.5);
+		glBegin(GL_POLYGON);
+
+		glVertex3f( -0.5, -0.5, -0.5);       // P1
+		glVertex3f( -0.5,  0.5, -0.5);       // P2
+		glVertex3f(  0.5,  0.5, -0.5);       // P3
+		glVertex3f(  0.5, -0.5, -0.5);       // P4
+
 		glEnd();
 		glutSwapBuffers();
 }
