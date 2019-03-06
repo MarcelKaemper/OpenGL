d04c3a137a3a7d30fa677266e5be1cfe6520630d
MarcelKaemper <marcelkaemper862@gmail.com>
2019-03-06 21:50:54 +0100

    Added movable dot

--- 2Dmovement.c
+++ 2Dmovement.c
@@ -4,6 +4,9 @@
 void render(void);
 void keyboard(unsigned char c, int x, int y);
 
+float xPos = 0.0f;
+float yPos = 0.0f;
+
 int main(int argc, char **argv){
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_RGBA);
@@ -19,6 +22,12 @@ int main(int argc, char **argv){
 
 void render(){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	glPointSize(6.0);
+	glBegin(GL_POINTS);
+	glVertex2f(xPos,yPos);
+	glEnd();
 
 	glutSwapBuffers();
 
@@ -26,7 +35,20 @@ void render(){
 
 void keyboard(unsigned char c, int x, int y){
 	if(c == 27){
-		exit(0);	
+		exit(0);
 	}
+	else if(c == 119){
+		yPos += 0.01;
+	}
+	else if(c == 115){
+		yPos -= 0.01;
+	}
+	else if(c == 97){
+		xPos -= 0.01;
+	}
+	else if(c == 100){
+		xPos += 0.01;
+	}
+	glutPostRedisplay();
 }
 
